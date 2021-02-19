a5c2de73665d878e1c2cf4044adecb9519e1f641
brandiw <brandimichelle.0911@gmail.com>
2021-02-19 15:31:29 -0800

    Adding starter code

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// HINT: You can delete this console.log after you no longer need it!
+console.log('JavaScript code has loaded!')
+
+// Global Variables - we'll use these to track the counts of each cookie type
+let gb = 0      // Ginger bread
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
+
+// Event listener for clicks on the "+" button for Ginger Bread cookies
+document.getElementById('add-gb').addEventListener('click', function() {
+    // HINT: You can delete this console.log after you no longer need it!
+    console.log('Ginger bread + button was clicked!')
+
+    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+})
+
+// TODO: Hook up event listeners for the rest of the buttons
