9e31daca52272c4cee422463849b96cd4243f77e
nkleinmann <nekleinmann@gmail.com>
2021-12-07 12:33:10 -0500

    Update starter code to use querySelector method

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,27 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
-
 // First, tell us your name
 let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let gb = 0 // Gingerbread
+let cc = 0 // Chocolate Chip
+let sugar = 0 // Sugar Sprinkle
 
-// Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+// selecting the element with an id of credit
+const credit = document.querySelector('#credit')
+// selecting the element with an id of add-gb
+const gbPlusBtn = document.querySelector('#add-gb')
+
+// Code to update name display
+credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+gbPlusBtn.addEventListener('click', function() {
+// HINT: You can delete this console.log after you no longer need it!
+console.log('Gingerbread + button was clicked!')
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
